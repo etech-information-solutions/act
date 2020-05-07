7b26583ef76bf76bcb218fad04a4a202dfc043d5
Tiaan <info@chrysaliscreative.co.za>
2020-05-07 16:51:35 +0200

    Updates to pallets

---
+++ ACT.Core/Enums/ActionType.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ACT.Core.Enums
+{
+    using Attributes;
+
+    [StringEnum]
+    public enum ActionType
+    {
+        [UiIgnoreEnumValue]       
+
+        [StringEnumDisplayText( "None" )]
+        None = 0,
+
+        [StringEnumDisplayText("Email")]
+        Email = 1,
+
+        [StringEnumDisplayText("Notify")]
+        Notify = 2,
+
+       [StringEnumDisplayText("Other")]
+        Other = 3
+    }
+}
